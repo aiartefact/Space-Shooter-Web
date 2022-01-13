fc5f2cbf34246890144d8a74ad44ce87f5be39b9
aiartefact <71671107+aiartefact@users.noreply.github.com>
2022-01-13 22:11:02 +0200

    Wave delay decrement over time; player and hazard speed increment over time

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -29,7 +29,14 @@ public class GameController : MonoBehaviour
     private int PrevScreenHeight = 768;
     private Vector3 PrevScoreTextPos = new Vector3(398,768,0);
 
+    // waveCount for waveWait change
+    private int waveCount;
 
+    // Movement speed increment
+    public PlayerController player;
+    private GameObject tempObject;
+    private Mover mover;
+    private float speedIncrement;
     private void Start()
     {
         gameOver = false;
@@ -51,6 +58,11 @@ public class GameController : MonoBehaviour
             PrevScreenHeight = Screen.height;
             scoreText.rectTransform.position = new Vector3(Screen.width * 0.295f, Screen.height, 0);
         }
+        // waveCount initialization
+        waveCount = 0;
+        // speedIncrement initialization
+        speedIncrement = 0;
+
         UpdateScore();
         StartCoroutine (SpawnWaves());
     }
@@ -89,15 +101,49 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(startWait);
         while (true)
         {
+            // TODO: Increment the bolt speed and fire rate for both player and enemies
+            // TODO: Intro with game description, rules and controls
+            
+            // waveCount increment
+            waveCount++;
+            // Movement speed increment every 4th wave
+            if (waveCount % 4 == 0)
+            {
+                speedIncrement += 0.5f;
+                // Player movement speed increment
+                player.speed += 0.5f;
+                // Decrease the delay before the next hazard every 8th wave until it reaches 0.05
+                if ((spawnWait > 0.11f) && (waveCount % 8 == 0)) // 0.11f is used to avoid comparison float to 0.1
+                {
+                    spawnWait -= 0.05f; // 0.05s for 1 speed unit
+                }
+            }
+
             for (int i = 0; i < hazardCount; i++)
             {
                 GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
+                //Instantiate(hazard, spawnPosition, spawnRotation);
+                tempObject = Instantiate(hazard, spawnPosition, spawnRotation);
+
+                // Movement speed increment 
+                mover = tempObject.GetComponent<Mover>();
+                mover.speed -= speedIncrement; // Hazard speed is negative for transform purposes
+
                 yield return new WaitForSeconds(spawnWait);
             }
-            yield return new WaitForSeconds(waveWait);
+
+            // Decrease the delay before the next wave every 3rd wave until it reaches 0
+            if (waveWait > 0.1f) // 0.1f is used to avoid comparison float to 0
+            {
+                if (waveCount % 3 == 0)
+                {
+                    waveWait -= 0.5f;                
+                }
+                yield return new WaitForSeconds(waveWait);
+            }
+            //yield return new WaitForSeconds(waveWait);
 
             if (gameOver)
             {
